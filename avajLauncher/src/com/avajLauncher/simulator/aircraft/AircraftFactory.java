8e11914a6f178b0d9c9c8dc8f66e18bcff1b9470
Ben Webb <bentenjamin@gmail.com>
2020-08-19 13:30:59 +0200

    created aircraftfactory class

---
+++ avajLauncher/src/com/avajLauncher/simulator/aircraft/AircraftFactory.java
@@ -0,0 +1,9 @@
+package com.avajLauncher.simulator.aircraft;
+
+import com.avajLauncher.simulator.Flyable;
+
+public abstract class AircraftFactory {
+    public static Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
+        
+    }
+}
