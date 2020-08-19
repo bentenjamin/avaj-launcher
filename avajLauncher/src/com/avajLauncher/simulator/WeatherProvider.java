604031af9adcc70ab5fbcb8456d69fafcbfb12b3
Ben Webb <bentenjamin@gmail.com>
2020-08-19 13:40:26 +0200

    created weatherprovider class

---
+++ avajLauncher/src/com/avajLauncher/simulator/WeatherProvider.java
@@ -0,0 +1,21 @@
+package com.avajLauncher.simulator;
+
+import com.avajLauncher.simulator.aircraft.Coordinates;
+
+public class WeatherProvider {
+    private static WeatherProvider weatherProvider;
+    private static String[] weather;
+
+    private WeatherProvider() {
+
+    }
+
+    public static WeatherProvider getProvider() {
+
+    }
+
+    public String getCurrentWeather(Coordinates coordinates) {
+        
+    }
+    
+}
